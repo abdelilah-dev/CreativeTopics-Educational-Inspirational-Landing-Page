9993909b721f6235ab1799928d73541977f9510d
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-04-17 17:36:02 +0100

    Upload the updates

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,6 @@ window.onscroll = function () {
     if (window.scrollY > 10) document.querySelector(".sticky-wrapper").classList.add("is-sticky");
     else document.querySelector(".sticky-wrapper").classList.remove("is-sticky");
 
-    // if (window.scrollY >= timelineSection.offsetTop - 200) {
-
-    //     timelineItems[0].classList.add("active");
-    //     if (window.scrollY >= timelineSection.offsetTop + 150) {
-    //         timelineItems[1].classList.add("active");
-    //     } else timelineItems[1].classList.remove("active");
-    //     if (window.scrollY >= timelineSection.offsetTop + 450) {
-    //         timelineItems[2].classList.add("active");
-    //     } else timelineItems[2].classList.remove("active");
-    // } else {
-    //     timelineItems[0].classList.remove("active");
-    // }
     timelineItems.forEach(item => {
         let itemOffsetTop = item.getBoundingClientRect().top + window.scrollY;
         if (window.scrollY >= itemOffsetTop - 300) {
@@ -32,6 +20,3 @@ window.onscroll = function () {
     });
 }
 
-function activeTimelineItems() {
-
-}
